84cb91b03128d6ff349c1f449360c182b661b663
siddharth <siddharthtiwarikreplind@gmail.com>
2024-03-18 20:05:38 +0530

    adding button for print

--- app/src/main/java/com/example/posprinter/MainActivity.java
+++ app/src/main/java/com/example/posprinter/MainActivity.java
@@ -1,74 +1,29 @@
 package com.example.posprinter;
 
 import android.content.*;
-import android.content.pm.PackageManager;
-import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.IBinder;
 import android.os.RemoteException;
-import android.util.Log;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
-import android.widget.ScrollView;
-import android.widget.TextView;
-import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
-import net.nyx.printerclient.aop.SingleClick;
 import net.nyx.printerservice.print.IPrinterService;
 import net.nyx.printerservice.print.PrintTextFormat;
 
-import java.text.DateFormat;
-import java.text.SimpleDateFormat;
-import java.util.Date;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
-import static com.example.posprinter.Result.msg;
-
-
-public class MainActivity extends AppCompatActivity implements View.OnClickListener {
-
-    private static final String TAG = "MainActivity";
-    protected Button btnVer;
-    protected Button btnPaper;
-    protected Button btn1;
-    protected Button btn2;
-    protected Button btn3;
-    protected Button btnScan;
-    protected TextView tvLog;
-
-    private static final int RC_SCAN = 0x99;
-    public static String PRN_TEXT;
-    protected Button btn4;
-    protected Button btnLbl;
-    protected Button btnLblLearning;
-
-    private ExecutorService singleThreadExecutor = Executors.newSingleThreadExecutor();
-    private Handler handler = new Handler();
-    String[] version = new String[1];
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        super.setContentView(R.layout.activity_main);
-        initView();
-        bindService();
-//        registerQscScanReceiver();
-        PRN_TEXT = getString(R.string.print_text);
-    }
-
+public class MainActivity extends AppCompatActivity {
 
     private IPrinterService printerService;
+    private ExecutorService singleThreadExecutor = Executors.newSingleThreadExecutor();
+    private Handler handler = new Handler();
     private ServiceConnection connService = new ServiceConnection() {
         @Override
         public void onServiceDisconnected(ComponentName name) {
-            showLog("printer service disconnected, try reconnect");
             printerService = null;
-            // 尝试重新bind
-            handler.postDelayed(() -> bindService(), 5000);
+            handler.postDelayed(MainActivity.this::bindService, 5000);
         }
 
         @Override
@@ -77,6 +32,30 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         }
     };
 
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button btnPaper = findViewById(R.id.btn_paper);
+        btnPaper.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                paperOut();
+            }
+        });
+
+        Button btnPrintText = findViewById(R.id.btn1);
+        btnPrintText.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                printText();
+            }
+        });
+
+        bindService();
+    }
+
     private void bindService() {
         Intent intent = new Intent();
         intent.setPackage("net.nyx.printerservice");
@@ -84,104 +63,16 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         bindService(intent, connService, Context.BIND_AUTO_CREATE);
     }
 
-    private void unbindService() {
-        unbindService(connService);
-    }
-
-    @SingleClick
-    @Override
-    public void onClick(View view) {
-       if (view.getId() == R.id.btn_paper) {
-            paperOut();
-        } else if (view.getId() == R.id.btn1) {
-            printText();
-        } else if (view.getId() == R.id.btn2) {
-            printBarcode();
-        } else if (view.getId() == R.id.btn3) {
-            printQrCode();
-        } else if (view.getId() == R.id.btn4) {
-            printBitmap();
-        }
-    }
-
     private void paperOut() {
-        singleThreadExecutor.submit(new Runnable() {
-            @Override
-            public void run() {
-                try {
-                    printerService.paperOut(80);
-                } catch (RemoteException e) {
-                    e.printStackTrace();
-                }
+        singleThreadExecutor.submit(() -> {
+            try {
+                printerService.paperOut(80);
+            } catch (RemoteException e) {
+                e.printStackTrace();
             }
         });
     }
 
-    private void printText() {
-        printText(PRN_TEXT);
-    }
-
-    private void printBarcode() {
-        singleThreadExecutor.submit(new Runnable() {
-            @Override
-            public void run() {
-                try {
-                    int ret = printerService.printBarcode("123456789", 300, 160, 1, 1);
-                    showLog("Print text: " + msg(ret));
-                    if (ret == 0) {
-                        paperOut();
-                    }
-                } catch (RemoteException e) {
-                    e.printStackTrace();
-                }
-            }
-        });
-    }
-
-    private void printQrCode() {
-        singleThreadExecutor.submit(new Runnable() {
-            @Override
-            public void run() {
-                try {
-                    int ret = printerService.printQrCode("123456789", 300, 300, 1);
-                    showLog("Print barcode: " + msg(ret));
-                    if (ret == 0) {
-                        paperOut();
-                    }
-                } catch (RemoteException e) {
-                    e.printStackTrace();
-                }
-            }
-        });
-    }
-
-    private void printBitmap() {
-        singleThreadExecutor.submit(new Runnable() {
-            @Override
-            public void run() {
-                try {
-                    int ret = printerService.printBitmap(BitmapFactory.decodeStream(getAssets().open("bmp.png")), 1, 1);
-                    showLog("Print bitmap: " + msg(ret));
-                    if (ret == 0) {
-                        paperOut();
-                    }
-                } catch (Exception e) {
-                    e.printStackTrace();
-                }
-            }
-        });
-    }
-
-    private void printKeyValueRightAligned(String key, String value, PrintTextFormat format) throws RemoteException {
-        final int totalWidth = 32; // Adjust based on your printer's capacity
-        int spacesNeededForKey = totalWidth - key.length() - value.length(); // Calculate space between key and value
-        String paddingForValue = ""; // Initialize the padding string
-        if (spacesNeededForKey > 0) {
-            paddingForValue = new String(new char[spacesNeededForKey]).replace('\0', ' '); // Create padding spaces
-        }
-        String formattedText = key + paddingForValue + value;
-        printerService.printText(formattedText + "\n", format); // Print the formatted string
-    }
 
     private void printKeyValueBelow(String key, String value) throws RemoteException {
         PrintTextFormat keyFormat = new PrintTextFormat();
@@ -195,26 +86,21 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         printerService.printText(value + "\n", valueFormat);
     }
 
-    private void printText(String customText) {
+    private void printText() {
         singleThreadExecutor.submit(new Runnable() {
             @Override
             public void run() {
                 try {
-                    // Assuming setting bold and underline for heading "BLINK POS"
                     PrintTextFormat headingFormat = new PrintTextFormat();
-                    headingFormat.setStyle(1); // Bold
+                    headingFormat.setStyle(1);
                     headingFormat.setUnderline(true);
-                    headingFormat.setAli(1); // Centered
+                    headingFormat.setAli(1);
                     headingFormat.setTextSize(32);
-                    printerService.printText("BLINK POS\n\n", headingFormat); // Print heading
+                    printerService.printText("BLINK POS\n\n", headingFormat);
 
-                    // Print each key-value pair with the value below the key
                     printKeyValueBelow("Username:", "Siddharth");
                     printKeyValueBelow("Amount:", "$12 USD");
                     printKeyValueBelow("Sats:", "12 Sats");
-                    printKeyValueBelow("Time:", new SimpleDateFormat("yyyy-MM-dd").format(new Date()));
-
-                    // Here, insert any additional logic needed for printing `customText` or handling the paper out
                     paperOut();
                 } catch (RemoteException e) {
                     e.printStackTrace();
@@ -223,90 +109,9 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         });
     }
 
-
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-        if (requestCode == RC_SCAN && resultCode == RESULT_OK && data != null) {
-            String result = data.getStringExtra("SCAN_RESULT");
-            showLog("Scanner result: " + result);
-        }
-    }
-
-    boolean existApp(String pkg) {
-        try {
-            return getPackageManager().getPackageInfo(pkg, 0) != null;
-        } catch (PackageManager.NameNotFoundException e) {
-            e.printStackTrace();
-        }
-        return false;
-    }
-
     @Override
     protected void onDestroy() {
         super.onDestroy();
-        unbindService();
-//        unregisterQscReceiver();
+        unbindService(connService);
     }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return super.onCreateOptionsMenu(menu);
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        if (item.getItemId() == R.id.menu_item_clear) {
-            clearLog();
-        }
-        return super.onOptionsItemSelected(item);
-    }
-
-    private void initView() {
-        btnPaper = (Button) findViewById(R.id.btn_paper);
-        btnPaper.setOnClickListener(MainActivity.this);
-        btn1 = (Button) findViewById(R.id.btn1);
-        btn1.setOnClickListener(MainActivity.this);
-        btn2 = (Button) findViewById(R.id.btn2);
-        btn2.setOnClickListener(MainActivity.this);
-        btn3 = (Button) findViewById(R.id.btn3);
-        btn3.setOnClickListener(MainActivity.this);
-        tvLog = (TextView) findViewById(R.id.tv_log);
-        btn4 = (Button) findViewById(R.id.btn4);
-        btn4.setOnClickListener(MainActivity.this);
-        btnLbl = (Button) findViewById(R.id.btn_lbl);
-        btnLbl.setOnClickListener(MainActivity.this);
-        btnLblLearning = (Button) findViewById(R.id.btn_lbl_learning);
-        btnLblLearning.setOnClickListener(MainActivity.this);
-    }
-
-    void showLog(String log, Object... args) {
-        if (args != null && args.length > 0) {
-            log = String.format(log, args);
-        }
-        String res = log;
-        Log.e(TAG, res);
-        DateFormat dateFormat = new SimpleDateFormat("MM-dd HH:mm:ss");
-        runOnUiThread(new Runnable() {
-            @Override
-            public void run() {
-                if (tvLog.getLineCount() > 100) {
-                    tvLog.setText("");
-                }
-                tvLog.append((dateFormat.format(new Date()) + ":" + res + "\n"));
-                tvLog.post(new Runnable() {
-                    @Override
-                    public void run() {
-                        ((ScrollView) tvLog.getParent()).fullScroll(View.FOCUS_DOWN);
-                    }
-                });
-            }
-        });
-    }
-
-    void clearLog() {
-        tvLog.setText("");
-    }
-
 }
